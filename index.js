07ab483f92dea88abc9c3ff3bb690de4d118f0a5
s0n0shee <rqbacud@up.edu.ph>
2024-05-19 22:21:04 +0800

    Frontend Update: changed to client requests
    - change button layout for choosing floors
    - set bounds to map panning (index js, lines 4-15)
    - changed color of map (index js, lines 33, 45, 58, 71)

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Import the leaflet package
 var L = require('leaflet');
 
+// Coordinates for the bounds (replace with actual coordinates of your campus area)
+var southWest = L.latLng(14.5790, 120.9830);
+var northEast = L.latLng(14.5820, 120.9860);
+var campusBounds = L.latLngBounds(southWest, northEast);
+
 // Creates a leaflet map binded to an html <div> with id "map"
 // setView will set the initial map view to the location at coordinates
 // 13 represents the initial zoom level with higher values being more zoomed in
-var map = L.map('map').setView([14.58025,120.98475], 20);
+var map = L.map('map', {
+    maxBounds: campusBounds,   // Set the max bounds to restrict panning
+    maxBoundsViscosity: 1.0,   // This makes sure the user can't pan outside the bounds
+}).setView([14.58025, 120.98475], 20);
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -22,7 +30,7 @@ L.circleMarker([14.58025,120.98475]).addTo(map);
 //initialize first floor map
 fetch('./map/casfloor1.geojson').then(response =>  response.json()).then(data => {
     L.geoJSON(data, {
-        style:{color: 'cadetblue',weight: 1,opacity: 1},
+        style:{color: '#517c34',weight: 1,opacity: 1},
     }).addTo(map);
     map.fitBounds(L.geoJSON(data).getBounds());
 }).catch(error => console.error('out of service.. ~_~  @_@', error));
@@ -34,7 +42,7 @@ document.getElementById('1').addEventListener('click', () => {
     });
     fetch('./map/casfloor1.geojson').then(response => response.json()).then(data => {
         L.geoJSON(data, {
-            style: { color: 'cadetblue', weight: 1, opacity: 1 },
+            style: { color: '#517c34', weight: 1, opacity: 1 },
         }).addTo(map);
         map.fitBounds(L.geoJSON(data).getBounds());
     }).catch(error => console.error('out of service.. ~_~  @_@', error));
@@ -47,7 +55,7 @@ document.getElementById('2').addEventListener('click', () => {
     });
     fetch('./map/casfloor2.geojson').then(response => response.json()).then(data => {
         L.geoJSON(data, {
-            style: { color: 'cadetblue', weight: 1, opacity: 1 },
+            style: { color: '#517c34', weight: 1, opacity: 1 },
         }).addTo(map);
         map.fitBounds(L.geoJSON(data).getBounds());
     }).catch(error => console.error('out of service.. ~_~  @_@', error));
@@ -60,7 +68,7 @@ document.getElementById('3').addEventListener('click', () => {
     });
     fetch('./map/casfloor3.2.geojson').then(response => response.json()).then(data => {
         L.geoJSON(data, {
-            style: { color: 'cadetblue', weight: 1, opacity: 1 },
+            style: { color: '#517c34', weight: 1, opacity: 1 },
         }).addTo(map);
         map.fitBounds(L.geoJSON(data).getBounds());
     }).catch(error => console.error('out of service.. ~_~  @_@', error));
